6948f754754cc896fd868466c7a2fa06afee4e72
git4ric <r6jain@uwaterloo.ca>
2016-04-07 04:27:56 -0400

    more testing

--- Clustering/src/main/scala/com/examples/TFIDFRepresentation.scala
+++ Clustering/src/main/scala/com/examples/TFIDFRepresentation.scala
@@ -119,7 +119,7 @@ object TFIDFRepresentation {
 		val hashingTF = new HashingTF()
 		
 		val tf = dataset.map(x => (x._1,hashingTF.transform(x._2))).cache()
-		val idf = new IDF(minDocFreq = 10).fit(tf.values)
+		val idf = new IDF(minDocFreq = 5).fit(tf.values)
 		
 		val tfidf = tf.map(x => (x._1,idf.transform(x._2))) 
 
