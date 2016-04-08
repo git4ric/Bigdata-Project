d9c361361097c4f107ac7417e1f010aa2b3e4cc6
git4ric <r6jain@uwaterloo.ca>
2016-04-08 00:58:58 -0400

    testing wiki on altiscale

--- Clustering/src/main/scala/com/examples/KMeansOld.scala
+++ Clustering/src/main/scala/com/examples/KMeansOld.scala
@@ -8,6 +8,14 @@ import org.apache.hadoop.fs._
 import org.rogach.scallop._
 import java.lang.Float
 
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.feature.IDF
+import org.apache.hadoop.io.{ LongWritable, Text }
+import org.apache.spark.rdd.RDD
+
 class Conf(args : Seq[ String ]) extends ScallopConf(args) {
 	mainOptions = Seq(input, output, clusters, iterations)
 	val input = opt[ String ](descr = "input path", required = true)
@@ -19,100 +27,174 @@ class Conf(args : Seq[ String ]) extends ScallopConf(args) {
 object KMeansOld {
 	val log = Logger.getLogger(getClass().getName())
 
+	def closestCentroid(vec : Map[ Int, Double ], centroids : Array[ Map[ Int, Double ] ]) : (Int,Map[ Int, Double ]) = {
+		var distance = Double.PositiveInfinity
+		var bestIndex = 0
+		for (i <- 0 until centroids.length) {
+			val tempDistance = cosineDistance(vec, centroids(i))
+			if (tempDistance < distance) {
+				distance = tempDistance
+				bestIndex = i
+			}
+		}
+		(bestIndex,centroids(bestIndex))
+	}
+
+	def averageDistanceBetweenCentroids(centroids : Array[ Map[ Int, Double ] ]) : Array[ Double ] = {
+
+		var result = new Array[ Double ](centroids.size)
+
+		for (i <- 0 until centroids.length) {
+			var distance_i = 0.0
+			for (j <- 0 until centroids.length) {
+				if (i != j) {
+					distance_i = distance_i + cosineDistance(centroids(i), centroids(j))
+				}
+			}
+			result.update(i, distance_i / centroids.size)
+		}
+		result
+	}
+
+	def dotProd(vec1 : Map[ Int, Double ], vec2 : Map[ Int, Double ]) : Double = {
+		var sum = 0.0
+
+		vec1.map(f => if (vec2.contains(f._1)) {
+			sum = sum + (f._2 * vec2.apply(f._1))
+		})
+		sum
+	}
+
+	def norm(vec : Map[ Int, Double ]) : Double = {
+		val dot = dotProd(vec, vec)
+		val result = math.sqrt(dot)
+		result
+	}
+
+	def cosineSimilarity(vec1 : Map[ Int, Double ], vec2 : Map[ Int, Double ]) : Double = {
+		dotProd(vec1, vec2) / (norm(vec1) * norm(vec2))
+	}
+
+	def cosineDistance(vec1 : Map[ Int, Double ], vec2 : Map[ Int, Double ]) : Double = {
+		val result = 1 - cosineSimilarity(vec1, vec2)
+		result
+	}
+
+	def mergeMap(vec1 : Map[ Int, Double ], vec2 : Map[ Int, Double ]) : Map[ Int, Double ] = {
+		val list = vec1.toList ++ vec2.toList
+		val merged = list.groupBy(_._1).map { case (k, v) => k -> v.map(_._2).sum }
+		merged
+	}
+
 	def main(argv : Array[ String ]) {
 
 		var logger = Logger.getLogger(this.getClass())
 
-		val jobName = "KMeansOld"
+		val jobName = "TFIDFRepresentation"
 
 		val conf = new SparkConf().setAppName(jobName)
 		val sc = new SparkContext(conf)
 		val args = new Conf(argv)
 		log.info("****** ~~~~~~ Input: " + args.input())
-	    log.info("****** ~~~~~~ Output: " + args.output())
-	    FileSystem.get(sc.hadoopConfiguration).delete(new Path(args.output()), true)
-	    
-	    val textFile = sc.textFile(args.input())
-	    				.map(line => {
-	    					val arr = line.split(",")
-	    					val x = Float.valueOf(arr(0).trim())
-	    					val y = Float.valueOf(arr(1).trim())
-	    					new DataPoint(x,y)
-	    				})
-	    				
-	    val dataPoints = textFile.toArray() 				
+		log.info("****** ~~~~~~ Output: " + args.output())
+		log.info("****** ~~~~~~ No. of Clusters: " + args.clusters())
+		log.info("****** ~~~~~~ No. of iterations: " + args.iterations())
+		FileSystem.get(sc.hadoopConfiguration).delete(new Path(args.output()), true)
 
-		val centroids = Array.fill(7) { DataPoint.random }
+		val hconf = new Configuration
+		hconf.set("textinputformat.record.delimiter", "#Article: ")
 
-		val resultCentroids = KMeansOld(dataPoints, centroids, 0.0001f)
-		log.info("***** resultCentroids size: " + resultCentroids.size)
-		val gg = sc.parallelize(resultCentroids)
-		
-		resultCentroids.foreach(f => log.info("" + f))
-		
-		// Group the points according to their distance to centroid
-		val cluster = dataPoints.groupBy { x =>
-			{
-				resultCentroids.reduceLeft(
-					(a, b) => if (x.EuclideanDistance(a) < x.EuclideanDistance(b)) {
-						a
-					}
-					else {
-						b
-					}
-				)
-			}
-		}
-		
-		log.info("***** Cluster size: " + cluster.size)
-		
-		cluster.foreach(f => log.info("" + f._1))
+		val dataset = sc.newAPIHadoopFile(args.input(), classOf[ TextInputFormat ], classOf[ LongWritable ], classOf[ Text ], hconf)
+			.map(x => x._2.toString())
+			.filter(x => x.isEmpty() == false)
+			.map(x => x.replaceAll("#Type: regular article", "")
+				.replaceAll("\\W", " ")
+				.replaceAll("\\s\\s+", " ")
+				.split(" ").filter(x => x.length() > 2).toSeq.drop(1))
+			
+//		val dataset = sc.textFile(args.input())
+//						.map(x => {
+//							val y = x.split("\\t")
+//							val z = y(0)
+//							val a = y(1).trim().split(" ").toSeq
+//							val b = a.filter(x => x.length() > 2 )
+//							(z,b)
+//						})
 				
-		gg.saveAsTextFile(args.output())
-	}
+//		println("Dataset: ")
+//		dataset.foreach(println)
 
-	def KMeansOld(points : Seq[ DataPoint ], centroids : Seq[ DataPoint ], delta : Float) : Seq[ DataPoint ] = {
+		val hashingTF = new HashingTF()
+		
+		val tf = dataset.map(x => (hashingTF.transform(x))).cache()
+		val idf = new IDF(minDocFreq = 5).fit(tf)
+		
+		val tfidf = tf.map(x => idf.transform(x)) 
 
-		// Group the points according to their distance to centroid
-		val cluster = points.groupBy { x =>
-			{
-				centroids.reduceLeft(
-					(a, b) => if (x.EuclideanDistance(a) < x.EuclideanDistance(b)) {
-						a
-					}
-					else {
-						b
-					}
-				)
-			}
-		}
-		
-		// Find new centroids but finding mean of all points in cluster
-		val newCentroids = centroids.map(centroid => {
+		val gg = tfidf.map(x => (x.toSparse))
+
+		val articles = gg.map(x => (x.indices zip x.values).toMap).cache()
+
+//		println("Articles: ")
+//		articles.foreach(println)
+
+		var centroids = articles.takeSample(false, args.clusters().toInt)
+
+//		println("Start centroids")
+//		println(centroids.deep.mkString("\n"))
+
+		var iteration = 1
+
+		while (iteration <= args.iterations().toInt) {
+
+			// Get the closest centroid to each article
+			// and map them as centroid -> (article,1)
+			val clusters = articles.map(article => (closestCentroid(article, centroids)._2, (article, 1)))
+
+			// Merge the articles and sum their occurrence within each 
+			// centroid cluster to create a new centroid
+			val newCentroids = clusters.reduceByKeyLocally({
+				case ((articleA, occurA), (articleB, occurB)) => (mergeMap(articleA, articleB), occurA + occurB)
+			})
+
+			// Divide each value of new centroid by cluster size to get mean	
+			val average = newCentroids.map({
+				case (centroid, (newCentroid, clusterSize)) =>
+					(centroid, newCentroid.map(x => (x._1, x._2 / clusterSize)))
+			})
+
+			// Update centroid	
+			var cent = centroids.map(oldCentroid => {
+				average.get(oldCentroid) match {
+					case Some(newCentroid) => newCentroid
+					case None => oldCentroid
+				}
+			})
 			
-			val clusterPoints = cluster.getOrElse(centroid, List())			
-			val clusterSize = clusterPoints.length
-			
-			if(clusterSize > 0)
+			// Compare cent and centroid to find convergence
+			val converge = (cent zip centroids).map{case (a,b) => cosineDistance(a,b)}
+
+			if(converge.exists(a => a > 0 && a < 0.000001))
 			{
-				clusterPoints.reduceLeft(_ + _) / clusterSize
+				println("***** ~~~~~  Converged in " + iteration.toString() + " iterations")
+				iteration = args.iterations().toInt;
 			}
-			else
-			{
-				centroid
-			}
-		})
-		
-		// Find out the change in centroids
-		val centroidChange = (cluster.zip(newCentroids)).map(f => f._1._1.EuclideanDistance(f._2))
-		
-		if(centroidChange.exists(_ < delta))
-		{
-			newCentroids
+
+			centroids = cent map(identity)
+			iteration = iteration + 1
+
 		}
-		else
-		{
-			KMeansOld(points, newCentroids, delta)
-		}
+		
+		val clusters = articles.map(article => (closestCentroid(article, centroids)._1,article))
+								
+		val numDocumentInClusters = clusters.groupByKey().map(f => (f._1,f._2.count(x => x.isEmpty == false )))
+		
+		println("***** ~~~~ Cluster -> No. of documents ")
+		println(numDocumentInClusters.toArray().mkString("\n"))
+//		
+//		val numClusterForDocuments = clusters.map(f => (f._2,f._1)).groupByKey()
+//											
+//		numClusterForDocuments.coalesce(1, false).saveAsTextFile(args.output())
+
 	}
 }
